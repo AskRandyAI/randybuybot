0c1475a5200f1b5a2e0aa282cd637f66eb93880a
AskRandyAI <askrandyai@gmail.com>
2026-01-11 11:12:07 -0500

    Feat: Add Gas Reserve in Calculator to prevent out-of-gas errors

--- utils/calculator.js
+++ utils/calculator.js
@@ -1,18 +1,28 @@
 const { FEE_PER_BUY_USD } = require('../config/constants');
+const price = require('./price');
+
+// Reserve ~$4 USD (approx 0.02 SOL) for gas fees/rent to prevent stuck wallets
+const GAS_RESERVE_USD = 4.00;
 
 function calculateCampaign(totalDepositUSD, numberOfBuys) {
-    const totalFees = numberOfBuys * FEE_PER_BUY_USD;
-    const availableForBuys = totalDepositUSD - totalFees;
-    const perBuyAmount = availableForBuys / numberOfBuys;
-    
-    // TODO: Get real-time SOL price for accurate conversion
-    const solPrice = 200; // Placeholder
-    const expectedDepositSOL = totalDepositUSD / solPrice;
-    
+    const totalServiceFees = numberOfBuys * FEE_PER_BUY_USD;
+
+    // Deduct Service Fees AND Gas Reserve
+    const availableForBuys = totalDepositUSD - totalServiceFees - GAS_RESERVE_USD;
+
+    let perBuyAmount;
+
+    if (availableForBuys <= 0) {
+        // Edge case for very small deposits
+        perBuyAmount = 0;
+    } else {
+        perBuyAmount = availableForBuys / numberOfBuys;
+    }
+
     return {
-        totalDeposit: totalDepositUSD,
-        totalFees: totalFees,
-        availableForBuys: availableForBuys,
+        totalFees: totalServiceFees,
+        gasReserve: GAS_RESERVE_USD,
+        availableForBuys: availableForBuys > 0 ? availableForBuys : 0,
         perBuyAmount: perBuyAmount,
         expectedDepositSOL: expectedDepositSOL
     };
